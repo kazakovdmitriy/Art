e79fa099095aac8f952ac90d19ae17a058241900
Dmitriy Kazakov <dmitriyfile@yandex.ru>
2025-01-28 16:15:42 +0300

    Initial Commit

---
+++ Art/ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtApp.swift
+//  Art
+//
+//  Created by Дмитрий Казаков on 28.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
