e5263c2eb88019165948fc7c7dc6f973c15f9eac
Dmitriy Kazakov <dmitriyfile@yandex.ru>
2025-01-28 16:20:34 +0300

    Initial

---
+++ Art/Views/ArtistsView.swift
@@ -0,0 +1,18 @@
+//
+//  ArtistsView.swift
+//  Art
+//
+//  Created by Дмитрий Казаков on 28.01.2025.
+//
+
+import SwiftUI
+
+struct ArtistsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArtistsView()
+}
